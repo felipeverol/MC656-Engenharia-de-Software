a6c679d4a124a63379be69751b7882452e9ddecf
Theo Maceres <macerestheo@gmail.com>
2025-09-09 15:16:33 -0300

    inserção da feature para listar produtos no carrinho

--- app/main.py
+++ app/main.py
@@ -10,3 +10,10 @@ def add_to_cart(barcode: str):
     if product:
         return {"msg": f"{product.code} added!", "cart": [p.name for p in cart.products]} 
     raise HTTPException(status_code=404, detail="Product not found")
+
+@app.get("/cart")
+def get_cart():
+    list_items = cart.list_items()
+    if list_items:
+        return {"cart": list_items}
+    raise HTTPException(status_code=404, detail="Cart not found")
