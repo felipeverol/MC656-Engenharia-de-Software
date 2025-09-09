d9479f1462a8c363d4ac7fa4d6609fae3a86a530
Felipe Verol <felipeverolrocha@gmail.com>
2025-09-09 14:11:22 -0300

    teste extra adicionado

--- test_main.py
+++ test_main.py
@@ -3,7 +3,7 @@ from app.main import app
 
 client = TestClient(app)
 
-def test_add_product():
+def test_add_product_found():
     """
     Testa o endpoint de adicionar um produto ao carrinho.
     Verifica o status code, o JSON retornado e o estado do carrinho.
@@ -17,3 +17,16 @@ def test_add_product():
     assert "msg" in data
     assert "cart" in data
     assert isinstance(data["cart"], list)
+
+def test_add_product_not_found():
+    """
+    Testa o endpoint de adicionar um produto ao carrinho com um código de barras inválido.
+    Verifica se o status code é 404 e a mensagem de erro correta.
+    """
+    invalid_barcode = "0000000000000"
+    response = client.get(f"/add/{invalid_barcode}")
+
+    assert response.status_code == 404, f"Erro: Status Code foi {response.status_code}"
+
+    data = response.json()
+    assert data["detail"] == "Product not found"
