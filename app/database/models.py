d82d78bfee0f01f905d3ffe897162636293f3b8c
Gabrielxouza7 <g269844@dac.unicamp.br>
2025-11-04 22:05:23 -0300

    adicionando bd

--- app/database/models.py
+++ app/database/models.py
@@ -1,6 +1,6 @@
-from sqlalchemy import Column, Integer, String, Float, ForeignKey
+from sqlalchemy import Column, Integer, String, Float, ForeignKey, JSON
 from sqlalchemy.orm import relationship
-from .database import Base
+from .database import Base 
 
 class User(Base):
     __tablename__ = "users"
@@ -8,22 +8,20 @@ class User(Base):
     name = Column(String, index=True)
     email = Column(String, unique=True, index=True)
     hashed_password = Column(String)
-    # Relação: Um usuário pode ter muitos carrinhos salvos
+    
+    # Relação: Um usuário pode ter muitas linhas de "SavedCart"
     carts = relationship("SavedCart", back_populates="owner")
 
 
 class Product(Base):
     __tablename__ = "products"
-    id = Column(Integer, primary_key=True, index=True)
-    barcode = Column(String, index=True)
+
+    barcode = Column(String, primary_key=True, index=True)
     name = Column(String, index=True)
     carbo = Column(Float, default=0.0)
     proteina = Column(Float, default=0.0)
     gordura = Column(Float, default=0.0)
     caloria = Column(Float, default=0.0)
-
-    # Relação: Este produto pode estar em muitos 'CartItems'
-    cart_items = relationship("SavedCarts", back_populates="product_details")
 
 
 class SavedCart(Base):
@@ -31,10 +29,6 @@ class SavedCart(Base):
 
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String, index=True, default="Meu Carrinho")
-    
-    # Chave estrangeira para o dono do carrinho
     user_id = Column(Integer, ForeignKey("users.id"))
-
-    # Relação: Aponta para o 'User' dono deste carrinho
+    cart_data = Column(JSON) 
     owner = relationship("User", back_populates="carts")
-
